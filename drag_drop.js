e71ba5060f9f173b8bfb55ad1ddb8d5f862881d4
吕雷 <958141982@qq.com>
2017-06-12 18:56:39 +0800

    add js

---
+++ drag_drop.js
@@ -0,0 +1,13 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("Text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("Text");
+    ev.target.appendChild(document.getElementById(data));
+}
